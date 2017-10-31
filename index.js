b9b43d3651aefff467eb3a62a4e395e2529e4c04
llxStar <961061833@qq.com>
2017-10-31 15:47:43 +0800

    判断url类型选择http还是https加载

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 // index.js is the entry of the project. 
 const http = require('http');
 const fs = require('fs');
+const https = require('https');//使用request可能会更简单点
+
+var url = 'http://www.imooc.com/video/15316';
+getUrlData(url);
 
 
-//获取http网站数据
-var url = '';
-http.get(url,function (res) {
-    var data = '';
-    res.on('data',function (chunk) {
-        data += chunk;
-    });
 
-    res.on('end',function () {
-        console.log('成功获取');
+function getUrlData(url) {
+    var h = http;
+    if(/^https/.test(url)){
+        h = https;
+    }
 
-        fs.writeFile('data.txt',data,function (err) {
-            if(err){
-                console.log('写入错误');
-                return;
-            }
+    h.get(url,function (res) {
+        var data = '';
+        res.on('data',function (chunk) {
+            data += chunk;
+        });
 
-            console.log('成功写入data.txt')
+        res.on('end',function () {
+            console.log('成功获取');
+
+            fs.writeFile('data.html',data,function (err) {
+                if(err){
+                    console.log('写入错误');
+                    return;
+                }
+
+                console.log('成功写入data.txt')
+            });
         });
     });
-});
+}
